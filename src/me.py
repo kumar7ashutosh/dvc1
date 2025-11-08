5b5b40048c6db4c5ff44c67bff3e7e14027f77aa
kumar7ashutosh <kumarashutoshbtech2023@gmail.com>
2025-11-08 17:25:40 +0530

    model evaluation

---
+++ src/me.py
@@ -0,0 +1,63 @@
+import numpy as np, pandas as pd,logging,os,pickle,json
+from sklearn.metrics import accuracy_score,roc_auc_score,precision_score,recall_score
+log_dir='logs'
+os.makedirs(log_dir,exist_ok=True)
+logger=logging.getLogger('model evaluation')
+logger.setLevel('DEBUG')
+
+console_handler=logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+log_file_path=os.path.join(log_dir,'model evaluation.log')
+file_handler=logging.FileHandler(log_file_path)
+file_handler.setLevel('DEBUG')
+
+formatter=logging.Formatter('%(asctime)s-%(name)s-%(levelname)s-%(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+def load_model(file_path:str):
+    with open (file_path,'rb') as file:
+        model=pickle.load(file)
+    logger.debug('model loaded from %s',file_path)
+    return model
+
+def load_data(file_path:str)->pd.DataFrame:
+    df=pd.read_csv(file_path)
+    logger.debug('data loaded from %s',file_path)
+    return df
+
+def evaluate_model(clf,x_test:np.ndarray,y_test:np.ndarray)->dict:
+    y_pred=clf.predict(x_test)
+    y_pred_proba=clf.predict_proba(x_test)[:,1]
+    accuracy=accuracy_score(y_test,y_pred)
+    precision=precision_score(y_test,y_pred)
+    recall=recall_score(y_test,y_pred)
+    auc=roc_auc_score(y_test,y_pred_proba)
+    
+    metrics_dict={
+        'accuracy':accuracy,
+        'precision':precision,
+        'recall':recall,
+        'auc':auc
+    }
+    return metrics_dict
+
+def save_metrics(metrics:dict,file_path:str)->None:
+    os.makedirs(os.path.dirname(file_path),exist_ok=True)
+    with open(file_path,'w') as file:
+        json.dump(metrics,file,indent=4)
+    
+def main():
+    clf=load_model('models/model.pkl')
+    test_data=load_data('data/transformed/test_tfidf.csv')
+    x_test=test_data.iloc[:,:-1].values
+    y_test=test_data.iloc[:,-1].values
+    metrics=evaluate_model(clf,x_test,y_test)
+    save_metrics(metrics,'report/metrics.json')
+    
+if __name__=='__main__':
+    main()
